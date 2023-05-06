c883614ac961b38e539a10de3ec193258a34aa37
Lorenzo Pezzotti <lorenzo.pezzotti01@gmail.com>
2023-05-06 15:07:44 +0200

    Make TBConstants constexpr

--- include/ATLLArBarrelTBConstants.hh
+++ include/ATLLArBarrelTBConstants.hh
@@ -44,39 +44,39 @@ namespace ATLLArBarrelTBConstants{
  
     //Geometry parameters for hits and sensitive detector
     //
-    const G4double EtaMin = 0.;                        //We consider only half barrel (z>0)
-    const G4double EtaMax = 1.475;                     //end of half barrel (z>0)
-    const G4double EtaChange = 0.8;                    //at eta=0.8 the absorber changes
-                                                       //the absorber is divided in
-                                                       //A (eta<0.8) and B (eta>0.8) sides 
-    const G4double RMin = 1.4*m;                       //Internal radius of whole calo
-    const G4double RMinSTAC = 1.5*m;                   //Internal radius of STAC
-    const G4double STAClength_eta0 = 470.*mm;          //STAC length at eta=0
-                                                       //At eta=0 the STAC is 22.3X0
-                                                       //front (4.3X0) + middle (16.0X0) + back (2.0X0)
-    const G4double X0 = 470./22.3;                     //X0 in mm
-    const G4double FrontDepth = 4.3*X0;                //Front section depth (mm)
-    const G4double MiddleDepth = FrontDepth + 16.0*X0; //Middle section depth (mm)
+    constexpr G4double EtaMin = 0.;                        //We consider only half barrel (z>0)
+    constexpr G4double EtaMax = 1.475;                     //end of half barrel (z>0)
+    constexpr G4double EtaChange = 0.8;                    //at eta=0.8 the absorber changes
+                                                           //the absorber is divided in
+                                                           //A (eta<0.8) and B (eta>0.8) sides 
+    constexpr G4double RMin = 1.4*m;                       //Internal radius of whole calo
+    constexpr G4double RMinSTAC = 1.5*m;                   //Internal radius of STAC
+    constexpr G4double STAClength_eta0 = 470.*mm;          //STAC length at eta=0
+                                                           //At eta=0 the STAC is 22.3X0
+                                                           //front (4.3X0) + middle (16.0X0) + back (2.0X0)
+    constexpr G4double X0 = 470./22.3;                     //X0 in mm
+    constexpr G4double FrontDepth = 4.3*X0;                //Front section depth (mm)
+    constexpr G4double MiddleDepth = FrontDepth + 16.0*X0; //Middle section depth (mm)
 
-    const G4double BackDeltaEta = 0.05;                //(rad)DeltaEta at back section
-                                                       //there are 16 eta bins in back section
-                                                       //up to eta=0.8 (first half calo eta<0.8)
-    const G4double MiddleDeltaEta = BackDeltaEta/2.;   //(rad)Eta bins in middle section
-    const G4double FrontDeltaEta  = MiddleDeltaEta/8.; //(rad)Eta bins in front section
+    constexpr G4double BackDeltaEta = 0.05;                //(rad)DeltaEta at back section
+                                                           //there are 16 eta bins in back section
+                                                           //up to eta=0.8 (first half calo eta<0.8)
+    constexpr G4double MiddleDeltaEta = BackDeltaEta/2.;   //(rad)Eta bins in middle section
+    constexpr G4double FrontDeltaEta  = MiddleDeltaEta/8.; //(rad)Eta bins in front section
 
-    const G4double STACDeltaPhi     = 2.*M_PI/16;      //(rad)Delta Phi STAC
-    const G4double halfSTACDeltaPhi = STACDeltaPhi/2.;
-    const G4double FrontDeltaPhi  = STACDeltaPhi/4;    //(rad)Delta Phi front section
-    const G4double MiddleDeltaPhi = STACDeltaPhi/16;   //(rad)Delta Phi middle section
-    const G4double BackDeltaPhi   = MiddleDeltaPhi;    //(rad)Delta Phi back section
+    constexpr G4double STACDeltaPhi     = 2.*M_PI/16;      //(rad)Delta Phi STAC
+    constexpr G4double halfSTACDeltaPhi = STACDeltaPhi/2.;
+    constexpr G4double FrontDeltaPhi  = STACDeltaPhi/4;    //(rad)Delta Phi front section
+    constexpr G4double MiddleDeltaPhi = STACDeltaPhi/16;   //(rad)Delta Phi middle section
+    constexpr G4double BackDeltaPhi   = MiddleDeltaPhi;    //(rad)Delta Phi back section
 
-    const G4int FrontHitNo  = 1024;                    //Number of hit to be allocated
-    const G4int MiddleHitNo = 512;                     //for front, middle and back
-    const G4int BackHitNo   = 256;                     //section (fist half calo eta<0.8)
+    constexpr G4int FrontHitNo  = 1024;                    //Number of hit to be allocated
+    constexpr G4int MiddleHitNo = 512;                     //for front, middle and back
+    constexpr G4int BackHitNo   = 256;                     //section (fist half calo eta<0.8)
     
-    const G4int BackEtasPerRow = 16;                   //Eta cells per row back section
-    const G4int MiddleEtasPerRow = BackEtasPerRow*2;   //Eta cells per row middle section
-    const G4int FrontEtasPerRow = MiddleEtasPerRow*8;  //Eta cells per row front section
+    constexpr G4int BackEtasPerRow = 16;                   //Eta cells per row back section
+    constexpr G4int MiddleEtasPerRow = BackEtasPerRow*2;   //Eta cells per row middle section
+    constexpr G4int FrontEtasPerRow = MiddleEtasPerRow*8;  //Eta cells per row front section
 
 } //ATLLArBarrelTBConstants
 
