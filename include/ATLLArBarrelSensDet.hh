8f18063de4b4ee746d67a3bb4340d62fec13783a
Lorenzo Pezzotti <lorenzo.pezzotti01@gmail.com>
2023-05-08 17:32:49 +0200

    Add Birks Law
    Adding Birks Law as single method by factoring out the relavant ATLAS
    Athena code. To be checked.

--- include/ATLLArBarrelSensDet.hh
+++ include/ATLLArBarrelSensDet.hh
@@ -58,10 +58,54 @@ class ATLLArBarrelSensDet : public G4VSensitiveDetector {
         //
         void CheckHitID(const ATLLArBarrelTBConstants::STACSection& Section, const G4int& HitID  ) const;
 
+        //Method to apply Birks law taken as it is in the ATLAS ATHENA code on 8 May 2023
+        //ATHENA LArG4BirksLaw class definition at
+        //https://gitlab.cern.ch/atlas/athena/-/blob/master/LArCalorimeter/LArG4/LArG4Code/LArG4Code/LArG4BirksLaw.h
+        //
+        G4double ApplyBirks(G4double dEMeV, G4double dXCm/*, G4double EFieldKVPerCm*/) const;
+
 };
 
 //Inline definition of class methods
 //
+inline G4double ATLLArBarrelSensDet::ApplyBirks(G4double dE, G4double dX/*, G4double EField*/) const {
+
+    /*Comments from ATLAS ATHENA reported as ATLASComment: ""*/
+
+    //LAr density for Birks Law taken from constructor of LArG4BirksLaw class constructor at
+    //https://gitlab.cern.ch/atlas/athena/-/blob/master/LArCalorimeter/LArG4/LArG4Barrel/src/LArBarrelCalculator.cxx#L85 
+    //
+    const G4double Birks_LAr_Density = 1.396;
+
+    //Birks Law k parameter taken from constructor of LArCalculatorSvcImp class at
+    //https://gitlab.cern.ch/atlas/athena/-/blob/master/LArCalorimeter/LArG4/LArG4Code/src/LArCalculatorSvcImp.cxx#L12
+    //
+    const G4double m_Birksk(0.05832);//ATLASComment:"value updated for G4 10.6.p03 - 1.2 times the previous value of 0.0486 used in all campaigns before MC21"
+    
+    //This value taken from ATHENA as passed to the actual m_BirksLaw function at
+    //https://gitlab.cern.ch/atlas/athena/-/blob/master/LArCalorimeter/LArG4/LArG4Barrel/src/LArBarrelCalculator.cxx#L253
+    //
+    const G4double efield = 10.; //ATLASComment: "kV/cm, assume constant for now"
+    
+    //From here on taken from m_BirksLaw ATHENA implementation at
+    //https://gitlab.cern.ch/atlas/athena/-/blob/master/LArCalorimeter/LArG4/LArG4Code/src/LArG4BirksLaw.cc#L7
+    //
+    if(dX < 1e-5) return dE; 
+    //if(EField<1e-3) return 0.;
+
+    G4double dEdX = dE/dX;
+
+    const G4double kOverField = m_Birksk/efield;
+
+    double dEcorr1 = dE * (1 + kOverField*1.51) / (1 + kOverField*dEdX/Birks_LAr_Density); //ATLASComment: "original corrections"
+
+    if (dEdX > 12000.0) dEdX = 12000.0; //ATLASComment: "The experimental data is available only until dE/dX~12000 MeV/cm"
+
+    double kHIP = (dEdX > 969.) ? 0.000754*dEdX+0.2692 : 1.0; //ATLASComment: "No corrections for dE/dX < 969 MeV/cm"
+
+    return dEcorr1*kHIP;
+}
+
 inline void ATLLArBarrelSensDet::CheckHitID(const ATLLArBarrelTBConstants::STACSection& Section, const G4int& HitID) const{
 
     switch(Section){
